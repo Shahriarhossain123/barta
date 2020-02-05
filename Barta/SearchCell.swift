b4b2709da0160566b4b7dc871805cca8097e0e72
Apple MacBook Pro <Apple@Apples-MacBook-Pro.local>
2020-02-05 11:19:07 +0600

    News2

---
+++ Barta/SearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchCell.swift
+//  Barta
+//
+//  Created by Apple MacBook Pro  on 2/5/20.
+//  Copyright © 2020 Apple MacBook Pro . All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
